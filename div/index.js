adeff4bfd75a16be0ce23b8cc67b759144539e60
ernies-dev <135020114+ernies-dev@users.noreply.github.com>
2023-06-30 13:20:17 +0000

    changing the default view

--- div/index.js
+++ div/index.js
@@ -1,20 +1,21 @@
 const darkModeToggle = document.querySelector("#dark-mode-btn");
 const lightModeToggle = document.querySelector("#light-mode-btn");
 const form = document.querySelector("#form");
-const btnDark = document.querySelector(".btnDark");
-
-darkModeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("darkmode");
-  form.classList.add("transparent");
-  lightModeToggle.classList.remove("activeMode");
-  darkModeToggle.classList.add("activeMode");
-  //   btnDark.classList.remove("btnDark");
-  //   btnDark.classList.add("specialBtn");
-});
+const signUpBtn = document.querySelector("#signUpBtn");
 
 lightModeToggle.addEventListener("click", () => {
   document.body.classList.toggle("darkmode");
+  form.classList.remove("transparent");
   darkModeToggle.classList.remove("activeMode");
   lightModeToggle.classList.add("activeMode");
+  signUpBtn.classList.remove("specialBtn");
+  signUpBtn.classList.add("darkBtn");
+});
+
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("darkmode");
+  lightModeToggle.classList.remove("activeMode");
+  darkModeToggle.classList.add("activeMode");
   form.classList.remove("transparent");
+  signUpBtn.classList.remove("darkBtn");
 });
